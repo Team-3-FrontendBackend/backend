cfab916cd5c2f9d433006693447fdede5d344570
Brennan Tanner <tan18010@byui.edu>
2022-03-15 11:13:39 -0600

    added routes in app.js

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const User = require("./models/user");
 const app = express();
 
 const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
+const liveRoutes = require("./routes/live");
 
 app.use(bodyParser.json()); // application/json
 
@@ -26,6 +28,8 @@ app.use((req, res, next) => {
 });
 
 app.use(authRoutes);
+app.use(adminRoutes);
+app.use(liveRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
