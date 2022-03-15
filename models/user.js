ce452f7ec959bced323e52785ccbe891d05815dc
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2022-03-15 10:36:44 -0600

    user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,28 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  siteName: {
+    type: String,
+    required: true,
+  },
+  globalData: {
+    ref: 'GlobalData',
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
