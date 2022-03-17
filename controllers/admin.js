5b146ee81b666aeba8f2bcf64287001722da1f0e
poyawang <poyawang12@gmail.com>
2022-03-17 10:35:21 -0600

    admin js created

---
+++ controllers/admin.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const { validationResult } = require('express-validator/check');
+
+const Admin = require('../models/globalData');
+
+//nav bar
+exports.getNav = (req,res,next) => {
+    res.status(200).json({ message: 'Get Navbar successfully' });
+}
+
+exports.addNav = (req, res, next) => {
+    const links = req.body.nav.links;
+    
+}
+
+exports.updateNav = (req, res, next) => {
+
+}
+
+exports.deleteNav = (req, res, next) => {
+
+}
+
+//header 
+
+exports.getHeader = (req, res, next) => {
+
+}
+
+exports.addHeader = (req, res, next) => {
+
+}
+
+exports.updateHeader = (req, res, next) => {
+
+}
+
+exports.deleteHeader = (req, res,next) => {
+
+}
+
+//footer
+exports.getFooter = (req, res, next) => {
+
+}
+
+exports.addFooter = (req, res, next) => {
+
+}
+
+exports.updateFooter = (req, res, next) => {
+
+}
+
+exports.deleteHeader = (req, res, next) => {
+
+}
+
