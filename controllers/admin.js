88ae2f0e2519c09d8a2903df9f22bdad9f101465
= <=>
2022-03-17 11:07:26 -0600

    working on getGlobalData

--- controllers/admin.js
+++ controllers/admin.js
@@ -25,11 +25,10 @@ exports.deleteNav = (req, res, next) => {
 //header 
 
 exports.getHeader = (req, res, next) => {
-
 }
 
 exports.addHeader = (req, res, next) => {
-
+    
 }
 
 exports.updateHeader = (req, res, next) => {
@@ -55,5 +54,35 @@ exports.updateFooter = (req, res, next) => {
 
 exports.deleteHeader = (req, res, next) => {
 
+}
+
+// globalData
+exports.getGlobalData = (req, res, next) => {
+
+    const errors = validationResult(req);
+    Admin.findById(req.userId).then(data => {
+        // output data
+        console.log(data);
+
+        // retrieve data from header
+        const headerLogoUrl = data.header.logoUrl;
+        const headerBackgroundColor = data.header.backgroundColor;
+
+        // retrieve data from nav
+        const navLinks = data.nav.links;
+
+        // retrieve data from footer
+        const footerContact = data.footer.contact;
+        const footerSocials = data.footer.socialLinks;
+
+        // return data as a json
+        res.status(200).json({
+            headerLogoUrl: headerLogoUrl, 
+            headerBackgroundColor: headerBackgroundColor,
+            navLinks: navLinks,
+            footerContact: footerContact,
+            footerSocials: footerSocials
+        })
+    });
 }
 
