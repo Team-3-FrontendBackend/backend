9cb656ea05a95fc415627b4f2e31d66767b74f15
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2022-03-22 12:06:44 -0600

    recreated users

--- frontend-test/test.js
+++ frontend-test/test.js
@@ -32,64 +32,64 @@ const herokuPutGlobalData =
 
 // signUp();
 
-// fetch(herokuLogin, {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify({
-//     username: 'Joe',
-//     password: '123456',
-//   }),
-// })
-//   .then((result) => {
-//     return result.json();
-//   })
-//   .then((result) => {
-//     jwt = result.token;
-//     console.log(jwt);
-//     putGlobalData();
-//     requestTwo();
-//   })
-//   .catch((err) => console.log(err));
+fetch(herokuLogin, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    username: 'Joe',
+    password: '123456',
+  }),
+})
+  .then((result) => {
+    return result.json();
+  })
+  .then((result) => {
+    jwt = result.token;
+    console.log(jwt);
+    putGlobalData();
+    requestTwo();
+  })
+  .catch((err) => console.log(err));
 
-// const requestTwo = () => {
-//   fetch(herokuGetGlobalData, {
-//     headers: {
-//       Authorization: 'Bearer ' + jwt.toString(),
-//     },
-//   })
-//     .then((result) => {
-//       return result.json();
-//     })
-//     .then((result) => console.log(result))
-//     .catch((err) => console.log(err));
-// };
+const requestTwo = () => {
+  fetch(herokuGetGlobalData, {
+    headers: {
+      Authorization: 'Bearer ' + jwt.toString(),
+    },
+  })
+    .then((result) => {
+      return result.json();
+    })
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err));
+};
 
-// const putGlobalData = () => {
-//   fetch(herokuPutGlobalData, {
-//     method: 'PUT',
-//     headers: {
-//       Authorization: 'Bearer ' + jwt.toString(),
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({
-//       header: {
-//         logoUrl:
-//           'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg',
-//         backgroundColor: '#eaeaea',
-//       },
-//       nav: {
-//         links: ['byui.edu', 'lds.org'],
-//       },
-//       footer: {
-//         contact: '503-270-8563',
-//         socialLinks: {
-//           facebook: 'facebook.com',
-//           iBelong: 'https://ibelong.byui.edu/',
-//           instagram: 'instagram.com',
-//         },
-//       },
-//     }),
-//   }).then((result) => console.log(result));
-// };
+const putGlobalData = () => {
+  fetch(herokuPutGlobalData, {
+    method: 'PUT',
+    headers: {
+      Authorization: 'Bearer ' + jwt.toString(),
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      header: {
+        logoUrl:
+          'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg',
+        backgroundColor: '#eaeaea',
+      },
+      nav: {
+        links: ['byui.edu', 'lds.org'],
+      },
+      footer: {
+        contact: '503-270-8563',
+        socialLinks: {
+          facebook: 'facebook.com',
+          iBelong: 'https://ibelong.byui.edu/',
+          instagram: 'instagram.com',
+        },
+      },
+    }),
+  }).then((result) => console.log(result));
+};
