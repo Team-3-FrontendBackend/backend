3e2066ccb48e48affec20edde490ea3b22b63493
= <=>
2022-03-29 10:16:50 -0600

    updating getHomePage

--- controllers/live.js
+++ controllers/live.js
@@ -6,46 +6,56 @@ const user = require("../models/user");
 exports.getHomePage = (req, res, next) => {
   const siteName = req.params.siteName;
 
-  User.findOne({ siteName: siteName }).then((user) => {
-    GlobalData.findOne({ userId: user._id }).then((data) => {
-      
-      // retrieve data from header
-      const headerLogoUrl = data.header.logoUrl;
-      const headerBackgroundColor = data.header.backgroundColor;
+  let headerLogoUrl;
+  let headerBackgroundColor;
+  let navLinks;
+  let footerContact;
+  let footerSocials;
+  let content;
+  let name;
 
-      // retrieve data from nav
-      const navLinks = data.nav.links;
+  User.findOne({ siteName: siteName })
+    .then((user) => {
+      return GlobalData.findOne({ userId: user._id });
+    })
+    .then((globalData) => {
+      // retrieve globalData from header
+      headerLogoUrl = globalData.header.logoUrl;
+      headerBackgroundColor = globalData.header.backgroundColor;
 
-      // retrieve data from footer
-      const footerContact = data.footer.contact;
-      const footerSocials = data.footer.socialLinks;
+      // retrieve globalData from nav
+      navLinks = globalData.nav.links;
 
-      Page.findOne({ url: siteName, userId: user._id }).then((page) => {
-        // error handling when no page is found
-        if (!page) {
-          const error = new Error("No page found");
-          error.statusCode = 404;
-          throw error;
-        }
+      // retrieve globalData from footer
+      footerContact = globalData.footer.contact;
+      footerSocials = globalData.footer.socialLinks;
 
-        // retrieve content in home page
-        const content = page.contentTemplates;
+      return Page.findOne({ url: siteName, userId: user._id });
+    })
+    .then((page) => {
+      // error handling when no page is found
+      if (!page) {
+        const error = new Error("No page found");
+        error.statusCode = 404;
+        throw error;
+      }
 
-        // retrieve the name of the page
-        const name = page.name;
+      // retrieve content in home page
+      content = page.contentTemplates;
 
-        // return data as a json
-        res.status(200).json({
-          headerLogoUrl: headerLogoUrl,
-          headerBackgroundColor: headerBackgroundColor,
-          navLinks: navLinks,
-          footerContact: footerContact,
-          footerSocials: footerSocials,
-          content: content,
-          name: name,
-        });
-      });
+      // retrieve the name of the page
+      name = page.name;
     });
+
+  // return data as a json
+  res.status(200).json({
+    headerLogoUrl: headerLogoUrl,
+    headerBackgroundColor: headerBackgroundColor,
+    navLinks: navLinks,
+    footerContact: footerContact,
+    footerSocials: footerSocials,
+    content: content,
+    name: name,
   });
 };
 
